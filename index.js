a687199c685d209514ed155619724d2edb7b0e88
Ritheesh <ritheesh.4@gmail.com>
2020-12-21 12:59:07 +0530

    page number autoselection bug fixed

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const services = require("./services");
 const scrapeDomains = require("./scrapeDomains");
 const { getDate } = require("./services");
+const cheerio = require("cheerio");
 
 (async () => {
 
-  const url = 'https://www.google.com/search?q=india+news'
+  const url = 'https://www.google.com/search?q=modern+languages'
   const filename = `./output/scrape-domains-${getDate()}.csv`;
   services.createFile(filename);
   let result = await services.getHtml(url);
@@ -17,11 +18,30 @@ const { getDate } = require("./services");
 
   const fetchNew = async (i) => {
     try {
-      await pageNumbers[i].click()
+
+      const currentPageNUmber = () => {
+
+        const $ = cheerio.load(html);
+        let childCount = 1;
+        while (i >= childCount) {
+          let pageNumber = $('td').eq(childCount).text()
+          console.log("i", i)
+          console.log("Page number", pageNumber)
+          if (i != pageNumber) {
+            childCount++
+          } else { break }
+        }
+        return childCount
+      }
+
+      console.log(currentPageNUmber())
+
+      await pageNumbers[currentPageNUmber()].click()
       var page = await browser.targets()[browser.targets().length - 1].page();
       const delay = 3 * 1000; // 1 minutes
       await new Promise((resolve) => setTimeout(resolve, delay));
       let currentHtml = await page.content();
+      html = currentHtml;
       let result = await scrapeDomains(currentHtml, filename);
       pageNumbers = await page.$$('table tr td');
       return result
@@ -30,9 +50,12 @@ const { getDate } = require("./services");
       return false
     }
   }
-  
-  for (let i = 2; i < 20; i++) {
-    await fetchNew(i)
+
+  for (let i = 2; i < 100; i++) {
+    let status = await fetchNew(i);
+    if(status === false){
+      break;
+    }
   }
   console.log('scrape completed')
 })();
